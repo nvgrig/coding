759a09533721e16fe9e3c4e41c4ef8bd130ff1e5
Nikita Grigoryev <nvgrigoryev@gmail.com>
2024-04-04 19:23:02 +0400

    Triangle solution

---
+++ src/main/java/com/nvgrig/coding/ya/Triangle.java
@@ -0,0 +1,18 @@
+package com.nvgrig.coding.ya;
+
+import java.util.Scanner;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int c = Integer.parseInt(scanner.nextLine());
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
